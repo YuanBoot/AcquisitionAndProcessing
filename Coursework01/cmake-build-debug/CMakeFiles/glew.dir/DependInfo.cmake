0f1d40e5f05fdf19373de8feaec2512a2fbeb1d7
yuanboot <nodeyuan@sina.com>
2018-01-26 12:19:00 +0000

    Init a new project

---
+++ Coursework01/cmake-build-debug/CMakeFiles/glew.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yuanbo/Developer/Github/AcquisitionAndProcessing/Coursework01/libigl/external/nanogui/ext/glew/src/glew.c" "/Users/yuanbo/Developer/Github/AcquisitionAndProcessing/Coursework01/cmake-build-debug/CMakeFiles/glew.dir/libigl/external/nanogui/ext/glew/src/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_BUILD"
+  "GLEW_NO_GLU"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../libigl/shared/cmake/../../external/nanogui/ext/glew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
